17a86ee6dca6f412ca48670ce9cf18b17f076671
Corwin Marsh <corwinm@users.noreply.github.com>
2020-04-11 19:54:17 -0700

    feat: Use red shirt profile image

--- src/components/profile-image.tsx
+++ src/components/profile-image.tsx
@@ -26,53 +26,10 @@ const ProfileImage: React.FC = () => {
           }
         }
       }
-      guestImage: file(relativePath: { eq: "profile-guest.jpg" }) {
-        childImageSharp {
-          fluid(maxWidth: 240, traceSVG: { color: "#C80303" }) {
-            ...GatsbyImageSharpFluid_tracedSVG
-          }
-        }
-      }
-      thinkingImage: file(relativePath: { eq: "profile-thinking.jpg" }) {
-        childImageSharp {
-          fluid(maxWidth: 240, traceSVG: { color: "#C80303" }) {
-            ...GatsbyImageSharpFluid_tracedSVG
-          }
-        }
-      }
-      weddingImage: file(relativePath: { eq: "profile-wedding.jpg" }) {
-        childImageSharp {
-          fluid(maxWidth: 240, traceSVG: { color: "#C80303" }) {
-            ...GatsbyImageSharpFluid_tracedSVG
-          }
-        }
-      }
     }
   `)
-  const [selected, setSelected] = React.useState<null | number>(null)
-  const update = (index: number) => {
-    if (selected) {
-      setSelected(null)
-    } else {
-      setSelected(index)
-    }
-  }
-  return (
-    <>
-      <div onClick={() => update(1)} hidden={!!selected && selected !== 1}>
-        <CircleImg fluid={data.engageImage.childImageSharp.fluid} />
-      </div>
-      <div onClick={() => update(2)} hidden={!!selected && selected !== 2}>
-        <CircleImg fluid={data.guestImage.childImageSharp.fluid} />
-      </div>
-      <div onClick={() => update(3)} hidden={!!selected && selected !== 3}>
-        <CircleImg fluid={data.thinkingImage.childImageSharp.fluid} />
-      </div>
-      <div onClick={() => update(4)} hidden={!!selected && selected !== 4}>
-        <CircleImg fluid={data.weddingImage.childImageSharp.fluid} />
-      </div>
-    </>
-  )
+
+  return <CircleImg fluid={data.engageImage.childImageSharp.fluid} />
 }
 
 export default ProfileImage
