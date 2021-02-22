876c2cf27a5e857efce463f94481e94951b695e7
Corwin Marsh <corwinm@users.noreply.github.com>
2021-02-21 21:35:53 -0800

    Package and content updates

---
+++ src/components/profile-link.tsx
@@ -0,0 +1,22 @@
+import * as React from "react"
+import styled from "styled-components"
+import { OutboundLink } from "gatsby-plugin-google-gtag"
+
+export const StyledProfileLink = styled(OutboundLink)`
+  color: ${(props) => props.theme.primary.link};
+  &:hover {
+    color: ${(props) => props.theme.primary.linkHover};
+  }
+`
+
+export const ProfileLink = ({
+  href,
+  children,
+}: {
+  href: string
+  children: React.ReactNode
+}) => (
+  <StyledProfileLink href={href} target="_blank">
+    {children}
+  </StyledProfileLink>
+)
